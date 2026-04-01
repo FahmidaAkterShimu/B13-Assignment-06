a5f4cafd3e9f8b7e681539a6d3a4aafbde27d2b4
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-04-01 22:37:21 +0600

    made navbar cart initially blank

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,8 +48,13 @@ const Navbar = ({ carts, setSelected }) => {
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                                 <div className="indicator">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+
                                     {/* Badge */}
-                                    <span className="badge badge-sm indicator-item rounded-full bg-red-600 p-1.5 text-white">{carts.length}</span>
+                                    <span className={`${carts.length === 0
+                                        ? "hidden"
+                                        : "badge badge-sm indicator-item rounded-full bg-red-600 p-1.5 text-white"}`}>
+                                        {carts.length === 0 ? null : carts.length}
+                                    </span>
                                 </div>
                             </div>
                             <div
@@ -57,7 +62,7 @@ const Navbar = ({ carts, setSelected }) => {
                                 className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
                                 <div className="card-body">
                                     <span className="text-lg font-bold">{carts.length} Items</span>
-                                 
+
                                     <div className="card-actions">
                                         <button
                                             onClick={() => setSelected("cart")}
